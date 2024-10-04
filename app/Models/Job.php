b60398f734fe24e13c30b25e2b40fc34382dfc56
SavindiRashmika <rashmikawadanambi>
2024-10-04 23:21:44 +0530

    updated

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,13 +3,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 
 class Job extends Model{
 
+    use HasFactory;
+
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
+    protected $fillable = ['employer_id','title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
 
 }   
